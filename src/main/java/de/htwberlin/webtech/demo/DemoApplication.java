b1dfde0deb6256d82789ef7006eaeb903c286e8a
alialagoez <alialagoez010@gmail.com>
2024-04-21 23:24:40 +0200

    Commit

--- src/main/java/de/htwberlin/webtech/demo/DemoApplication.java
+++ src/main/java/de/htwberlin/webtech/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) { //sddasdasd
+	public static void main(String[] args) { //sddasdasda
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
