ab954094b0800646f637ffdf8819a196716072f0
alialagoez <alialagoez010@gmail.com>
2024-04-23 15:54:47 +0200

    blabittes

--- src/main/java/de/htwberlin/webtech/demo/DemoApplication.java
+++ src/main/java/de/htwberlin/webtech/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) { //d
+	public static void main(String[] args) { //dbladde
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
