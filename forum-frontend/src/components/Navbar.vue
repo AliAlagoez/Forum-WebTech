3592305d1d6293d3b0b9b98941e6e96f167b2fea
AMinato77 <amin.tekaya@gmail.com>
2024-05-12 22:35:28 +0200

    Json3

--- forum-frontend/src/components/Navbar.vue
+++ forum-frontend/src/components/Navbar.vue
@@ -9,7 +9,7 @@
         <div class="navbar-nav">
           <router-link class="nav-link" to="/">Home</router-link>
           <router-link class="nav-link" to="/about">About</router-link>
-          <router-link class="nav-link" to="/posts">Post</router-link>
+          <router-link class="nav-link" to="/post">Post</router-link>
         </div>
       </div>
     </div>
